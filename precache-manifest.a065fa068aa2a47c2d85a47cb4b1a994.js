b1ff615bd66d32a99bb7aec923338270478a4fbf
faisal0907 <106477789+faisal0907@users.noreply.github.com>
2022-10-10 08:13:19 +0500

    Updates

---
+++ precache-manifest.a065fa068aa2a47c2d85a47cb4b1a994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da0f016f970da9c998a9",
+    "url": "/my-app/static/css/main.0828aed4.chunk.css"
+  },
+  {
+    "revision": "da0f016f970da9c998a9",
+    "url": "/my-app/static/js/main.da0f016f.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/my-app/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "03f09d9637ca08bef699",
+    "url": "/my-app/static/js/runtime~main.03f09d96.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "eaa57aa4527f57d45bb04e3d52c2a61d",
+    "url": "/my-app/index.html"
+  }
+];
